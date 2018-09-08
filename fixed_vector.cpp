0fe318c9459c20a1504e6d1cb75fe3283a0701ba
vlad <kalugin.vlad@gmail.com>
2018-09-08 14:56:39 +0300

    start

---
+++ fixed_vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vladislav Kalugin on 08/09/2018.
+//
+
+#include "fixed_vector.h"
---
+++ fixed_vector.h
@@ -0,0 +1,14 @@
+//
+// Created by Vladislav Kalugin on 08/09/2018.
+//
+
+#ifndef FIXED_VECTOR_FIXED_VECTOR_H
+#define FIXED_VECTOR_FIXED_VECTOR_H
+
+
+class fixed_vector {
+
+};
+
+
+#endif //FIXED_VECTOR_FIXED_VECTOR_H
